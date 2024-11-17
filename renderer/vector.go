d32446ad373c9f16f63576d9c4ae98b9b6749447
Andrew Blackwell <118554835+AndrewBlackwell@users.noreply.github.com>
2024-11-17 17:42:09 -0500

    More Vector Operations

--- renderer/vector.go
+++ renderer/vector.go
@@ -1,13 +1,36 @@
 package renderer
 
+// Vector represents a 3D vector with X, Y, and Z components.
 type Vector struct {
 	X, Y, Z float64
 }
 
+// Creates a new vector with the given X, Y, and Z.
 func NewVector(x, y, z float64) Vector {
 	return Vector{x, y, z}
 }
 
+// Creates a new vector with the given scalar value for all components.
 func NewVectorFromScalar(scalar float64) Vector {
 	return Vector{X: scalar, Y: scalar, Z: scalar}
+}
+
+// Vector Addiditon
+func (a Vector) Add(b Vector) Vector {
+	return NewVector(a.X+b.X, a.Y+b.Y, a.Z+b.Z)
+}
+
+// Vector Subtraction
+func (a Vector) Sub(b Vector) Vector {
+	return NewVector(a.X-b.X, a.Y-b.Y, a.Z-b.Z)
+}
+
+// Scalar Multiplication
+func (a Vector) ScalarMult(scalar float64) Vector {
+	return NewVector(a.X*scalar, a.Y*scalar, a.Z*scalar)
+}
+
+// Scalar Multiplication
+func (a Vector) ScalarDiv(scalar float64) Vector {
+	return NewVector(a.X/scalar, a.Y/scalar, a.Z/scalar)
 }
